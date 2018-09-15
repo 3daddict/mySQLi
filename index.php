e162f801d29d06f2288bc24eec96cd22eaeeaaa8
Michael Salvati <print@3daddict.com>
2018-09-15 14:25:54 -0700

    basic query and read data setup

--- index.php
+++ index.php
@@ -2,9 +2,34 @@
 require_once('mysql_connect.php');
 print("conn: "); print_r($conn);
 
-$query = 'SELECT * FROM users';
+$query = 'SELECT id, username, email FROM users';
 
 $result = mysqli_query($conn,$query);
 
-print("<br><br><pre>.print_r"($result,true).'<pre>');
+print("<br><br><pre>".print_r($result,true).'<pre>');
+
+if(!empty($result) && mysqli_num_rows($result) > 0) {
+    print("<br><br>we have results");
+    while($single_row = mysqli_fetch_assoc($result)) {
+        print("<br>got a row".print_r($single_row,true));
+    }
+}
+
+$username = 'Willy';
+$password = md5('wonka');
+$email = 'willy@wonka.com';
+$query2 = "INSERT INTO `users` SET
+    `username` = '$username',
+    `password` = '$password',
+    `email` = '$email'";
+
+print("<br>query = $query2");
+$result = mysqli_query($conn,$query2);
+    if(mysqli_affected_rows($result) > 0) {
+        print("<br>Row was inserted");
+    } else {
+        print("something went wrong");
+    }
+    print(mysqli_insert_id($conn));
+
 ?>
